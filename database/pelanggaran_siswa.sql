a2b31da51ad8124207eb591bd82c5507c1c308ce
Akbar Bagus Wicaksana <110794387+Akbarbagusw@users.noreply.github.com>
2024-01-22 11:22:01 +0700

    first commit

---
+++ database/pelanggaran_siswa.sql
@@ -0,0 +1,213 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Waktu pembuatan: 22 Jan 2024 pada 05.06
+-- Versi server: 10.4.28-MariaDB
+-- Versi PHP: 8.2.4
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `pelanggaran_siswa`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `detail_pelanggaran_siswa`
+--
+
+CREATE TABLE `detail_pelanggaran_siswa` (
+  `id_pelanggaran_siswa` int(11) NOT NULL,
+  `id_pelanggaran` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data untuk tabel `detail_pelanggaran_siswa`
+--
+
+INSERT INTO `detail_pelanggaran_siswa` (`id_pelanggaran_siswa`, `id_pelanggaran`) VALUES
+(20, 2);
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `pelanggaran`
+--
+
+CREATE TABLE `pelanggaran` (
+  `id_pelanggaran` int(11) NOT NULL,
+  `nama_pelanggaran` varchar(300) NOT NULL,
+  `poin` double NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data untuk tabel `pelanggaran`
+--
+
+INSERT INTO `pelanggaran` (`id_pelanggaran`, `nama_pelanggaran`, `poin`) VALUES
+(2, 'telat', 20);
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `pelanggaran_siswa`
+--
+
+CREATE TABLE `pelanggaran_siswa` (
+  `id_pelanggaran_siswa` int(11) NOT NULL,
+  `waktu` datetime NOT NULL,
+  `id_siswa` int(11) NOT NULL,
+  `id_user` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data untuk tabel `pelanggaran_siswa`
+--
+
+INSERT INTO `pelanggaran_siswa` (`id_pelanggaran_siswa`, `waktu`, `id_siswa`, `id_user`) VALUES
+(20, '2024-01-10 13:19:39', 8, 4);
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `siswa`
+--
+
+CREATE TABLE `siswa` (
+  `id_siswa` int(11) NOT NULL,
+  `nis` varchar(20) NOT NULL,
+  `nama_siswa` varchar(300) NOT NULL,
+  `kelas` varchar(20) NOT NULL,
+  `poin` double NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data untuk tabel `siswa`
+--
+
+INSERT INTO `siswa` (`id_siswa`, `nis`, `nama_siswa`, `kelas`, `poin`) VALUES
+(5, '1782937', 'Ardhito', 'XI RPL 1', 0),
+(6, '6780', 'Ridho Ilham Dwi Subekti', 'XI RPL 1', 15),
+(8, '1782937', 'Ardhito', 'XI RPL 1', 0),
+(9, '1782937', 'Ardhito', 'XI RPL 1', 0);
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `user`
+--
+
+CREATE TABLE `user` (
+  `id_user` int(11) NOT NULL,
+  `nama_user` varchar(300) NOT NULL,
+  `username` varchar(300) NOT NULL,
+  `password` varchar(300) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data untuk tabel `user`
+--
+
+INSERT INTO `user` (`id_user`, `nama_user`, `username`, `password`) VALUES
+(4, 'P Firman', 'kesiswaan', 'e034fb6b66aacc1d48f445ddfb08da98'),
+(5, 'P Nasiqin', 'BK', '827ccb0eea8a706c4c34a16891f84e7b'),
+(6, 'bayu', 'bayu', 'a430e06de5ce438d499c2e4063d60fd6');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indeks untuk tabel `detail_pelanggaran_siswa`
+--
+ALTER TABLE `detail_pelanggaran_siswa`
+  ADD KEY `id_pelanggaran` (`id_pelanggaran`),
+  ADD KEY `id_pelanggaran_siswa` (`id_pelanggaran_siswa`);
+
+--
+-- Indeks untuk tabel `pelanggaran`
+--
+ALTER TABLE `pelanggaran`
+  ADD PRIMARY KEY (`id_pelanggaran`);
+
+--
+-- Indeks untuk tabel `pelanggaran_siswa`
+--
+ALTER TABLE `pelanggaran_siswa`
+  ADD PRIMARY KEY (`id_pelanggaran_siswa`),
+  ADD KEY `id_siswa` (`id_siswa`,`id_user`),
+  ADD KEY `id_user` (`id_user`);
+
+--
+-- Indeks untuk tabel `siswa`
+--
+ALTER TABLE `siswa`
+  ADD PRIMARY KEY (`id_siswa`);
+
+--
+-- Indeks untuk tabel `user`
+--
+ALTER TABLE `user`
+  ADD PRIMARY KEY (`id_user`);
+
+--
+-- AUTO_INCREMENT untuk tabel yang dibuang
+--
+
+--
+-- AUTO_INCREMENT untuk tabel `pelanggaran`
+--
+ALTER TABLE `pelanggaran`
+  MODIFY `id_pelanggaran` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT untuk tabel `pelanggaran_siswa`
+--
+ALTER TABLE `pelanggaran_siswa`
+  MODIFY `id_pelanggaran_siswa` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=21;
+
+--
+-- AUTO_INCREMENT untuk tabel `siswa`
+--
+ALTER TABLE `siswa`
+  MODIFY `id_siswa` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
+
+--
+-- AUTO_INCREMENT untuk tabel `user`
+--
+ALTER TABLE `user`
+  MODIFY `id_user` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+
+--
+-- Ketidakleluasaan untuk tabel pelimpahan (Dumped Tables)
+--
+
+--
+-- Ketidakleluasaan untuk tabel `detail_pelanggaran_siswa`
+--
+ALTER TABLE `detail_pelanggaran_siswa`
+  ADD CONSTRAINT `detail_pelanggaran_siswa_ibfk_1` FOREIGN KEY (`id_pelanggaran`) REFERENCES `pelanggaran` (`id_pelanggaran`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `detail_pelanggaran_siswa_ibfk_2` FOREIGN KEY (`id_pelanggaran_siswa`) REFERENCES `pelanggaran_siswa` (`id_pelanggaran_siswa`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Ketidakleluasaan untuk tabel `pelanggaran_siswa`
+--
+ALTER TABLE `pelanggaran_siswa`
+  ADD CONSTRAINT `pelanggaran_siswa_ibfk_1` FOREIGN KEY (`id_siswa`) REFERENCES `siswa` (`id_siswa`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `pelanggaran_siswa_ibfk_2` FOREIGN KEY (`id_user`) REFERENCES `user` (`id_user`) ON DELETE CASCADE ON UPDATE CASCADE;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
